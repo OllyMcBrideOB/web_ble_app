e2a466c2226ae37708d700e615d9aa9b80bbda57
Olly McBride OB <ollymcbride@openbionics.com>
2021-08-27 14:31:22 +0100

    UART TX works, UART Rx not yet displaying correctly

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ var bluetoothDeviceDetected;
             }
         },
         UART: {
-            UUID: "0b0b1006-feed-dead-bee5-0be9b1091c50",                   // BLE_UUID_UART_SERVICE
+            UUID: "6e400001-b5a3-f393-e0a9-e50e24dcca9e",                   // NUS
             Rx: {
-                UUID: "0b0b2015-feed-dead-bee5-0be9b1091c50",           // BLE_UUID_RX_CHAR
+                UUID: "6e400002-b5a3-f393-e0a9-e50e24dcca9e",           // NUS Rx
             },
             Tx: {
-                UUID: "0b0b2016-feed-dead-bee5-0be9b1091c50"            // BLE_UUID_TX_CHAR
+                UUID: "6e400003-b5a3-f393-e0a9-e50e24dcca9e"            // NUS Tx
             }  
         }
     }
@@ -43,9 +43,11 @@ var bluetoothDeviceDetected;
 
     document.getElementById("slider").addEventListener("input", function(event) {
         document.getElementById("label_slider").innerHTML = this.value;
-        document.getElementById("console_rx").value = this.value
-        write(GATT.UART.Rx)
-        // if (isWebBLEAvailable()) { stop() }
+        if (isWebBLEAvailable()) { write(GATT.UART.Rx) }
+    })
+
+    document.getElementById("btn_uart_enter").addEventListener("click", function(event) {
+        writeToUART(document.getElementById("uart_tx").value);
     })
 
     function isWebBLEAvailable() {
@@ -58,7 +60,7 @@ var bluetoothDeviceDetected;
 
     function getDeviceInfo() {
         let options = {
-            optionalServices: [GATT.nonRealTime.UUID, GATT.realTime.UUID],
+            optionalServices: [GATT.nonRealTime.UUID, GATT.realTime.UUID, GATT.UART.UUID],
             filters: [
                 { name: deviceName }
             ]
@@ -77,68 +79,169 @@ var bluetoothDeviceDetected;
         .then(connectGATT)
         .then(_ => {
             console.log("Reading val...");
-            return GATT.realTime.RTButtonStatus.handle.readValue();
         })
         .catch(error => {
-            console.log("Waiting to start reading..." + error);
+            console.log("[ERROR line: " + error.lineNumber + "] read(): " + error)
         })
     }
 
-    function connectGATT() {
-        if(bluetoothDeviceDetected.gatt.connected && characteristics.RTButtonStatus.handle) {
-            return Promise.resolve();
-        }
+function connectGATT() {
+    if(bluetoothDeviceDetected.gatt.connected) {
+        return Promise.resolve();
+    }
 
-        return bluetoothDeviceDetected.gatt.connect()
-        .then(server => {
-            console.log("Getting GATT Service...");
-            return server.getPrimaryService(GATT.realTime.UUID);
-        })
+    var gatt_server;
+
+    return bluetoothDeviceDetected.gatt.connect()
+    .then(server => {
+        console.log("Discovering Services...");
+
+        gatt_server = server;
+    })
+    // discover NRT Service
+    .then(() => {
+        // discover NRT characteristics
+        return gatt_server.getPrimaryService(GATT.nonRealTime.UUID)
+        // store service handle
         .then(service => {
-            console.log("Getting GATT Characteristic...");
-            return service.getCharacteristic(GATT.realTime.RTButtonStatus.UUID);
+            GATT.nonRealTime.handle = service;
         })
-        .then(characteristic => {
-            GATT.realTime.RTButtonStatus.handle = characteristic;
-            GATT.realTime.RTButtonStatus.handle.addEventListener("characteristicvaluechanged", handleChangedValue);
-            start();
+        // discover nonRealTime NRTRequest char
+        .then(() => {
+            return GATT.nonRealTime.handle.getCharacteristic(GATT.nonRealTime.NRTRequest.UUID)
+            // store nonRealTime NRTRequest handle
+            .then(characteristic => {
+                GATT.nonRealTime.NRTRequest.handle = characteristic;
+            })
+        })
+        // discover nonRealTime NRTResponse char
+        .then(() => {
+            return GATT.nonRealTime.handle.getCharacteristic(GATT.nonRealTime.NRTResponse.UUID)
+            // store nonRealTime NRTResponse handle & add event listener to nonRealTime NRTResponse
+            .then(characteristic => {
+                GATT.nonRealTime.NRTResponse.handle = characteristic;
+                GATT.nonRealTime.NRTResponse.handle.addEventListener("characteristicvaluechanged", onNRTResponse);
+            })
+        })
+    })
+    // discover RT Service
+    .then(() => {
+        return gatt_server.getPrimaryService(GATT.realTime.UUID)
+        // store service handle
+        .then(service => {
+            GATT.realTime.handle = service;
+        })
+        // discover RTButtonStatus char
+        .then(() => {
+            return GATT.realTime.handle.getCharacteristic(GATT.realTime.RTButtonStatus.UUID)
+            // store RTButtonStatus char & add event listener to ButtonStatus char
+            .then(characteristic => {
+                GATT.realTime.RTButtonStatus.handle = characteristic;
+                GATT.realTime.RTButtonStatus.handle.addEventListener("characteristicvaluechanged", onRTButtonStatusChange);
+            })
+        })
+    })
+    // discover UART Service
+    .then(() => {
+        // discover UART characteristics
+        return gatt_server.getPrimaryService(GATT.UART.UUID)
+        // store service handle
+        .then(service => {
+            GATT.UART.handle = service;
+        })
+        // discover UART Rx char
+        .then(() => {
+            return GATT.UART.handle.getCharacteristic(GATT.UART.Rx.UUID)
+            // store UART Rx handle
+            .then(characteristic => {
+                GATT.UART.Rx.handle = characteristic;
+            })
+        })
+        // discover UART Tx char
+        .then(() => {
+            return GATT.UART.handle.getCharacteristic(GATT.UART.Tx.UUID)
+            // store UART Tx handle & add event listener to UART Tx
+            .then(characteristic => {
+                GATT.UART.Tx.handle = characteristic;
+                GATT.UART.Tx.handle.addEventListener("characteristicvaluechanged", onUARTReceived);
+            })
+        })
+    })
 
-            document.getElementById("btn_start").disabled = false;
-            document.getElementById("btn_stop").disabled = true;
-        })
-    }
+    // Start the BLE notifications
+    .then(() => {
+        start()
+        document.getElementById("btn_start").disabled = false;
+        document.getElementById("btn_stop").disabled = true;
+    })
+}
 
-    function write(characteristic, val_to_write) {
-        return characteristic.handle.writeValue(val_to_write)
+function str2ab(str) {
+    var buf = new ArrayBuffer(str.length);
+    var bufView = new Uint8Array(buf);
+    for (var i=0, strLen=str.length; i < strLen; i++) {
+      bufView[i] = str.charCodeAt(i);
     }
+    return buf;
+  }
 
-    function handleChangedValue(event) {
-        let val = event.target.value.getUint8(0);
-        var now = new Date();
-        console.log("> " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + " Val is " + val)
-        document.getElementById("console_rx").value = val.toString();
-    }
-    
-    function start() {
-        GATT.realTime.RTButtonStatus.handle.startNotifications()
-        .then(_ => {
-            console.log("Start reading...");
-            document.getElementById("btn_start").disabled = true;
-            document.getElementById("btn_stop").disabled = false;
-        })
-        .catch(error => {
-            console.log("[ERROR] Start: " + error)
-        })
-    }
+function write(characteristic, val_to_write) {
+    return characteristic.handle.writeValue(str2ab(val_to_write))       // ArrayBuffer
+}
 
-    function stop() {
-        GATT.realTime.RTButtonStatus.handle.stopNotifications()
-        .then(_ => {
-            console.log("Stop reading...");
-            document.getElementById("btn_start").disabled = false;
-            document.getElementById("btn_stop").disabled = true;
-        })
-        .catch(error => {
-            console.log("[ERROR] Stop: " + error)
-        })
+function onRTButtonStatusChange(event) {
+    let val = event.target.value.getUint8(0);
+    document.getElementById("label_btnstatus").innerHTML = val.toString();
+}
+
+function onUARTReceived(event) {
+    let val = event.target.value;
+    var now = new Date();
+    console.log("> " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + " onUARTTxChange is " + val)
+    document.getElementById("uart_rx").value = val;
+}
+
+function writeToUART(value) {
+    if (bluetoothDeviceDetected)
+    {
+        console.log("Writing '" + value + "' to UART");
+        write(GATT.UART.Rx, value);
     }
+    else
+    {
+        console.log("ERROR - Failed to write '" + value + "' to UART as BLE not connected")
+    }
+}
+
+function onNRTResponse(event) {
+    let val = event.target.value;
+    var now = new Date();
+    console.log("> " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + " onNRTResponse is " + val)
+    document.getElementById("uart_rx").value = val;
+}
+
+function start() {
+    GATT.realTime.RTButtonStatus.handle.startNotifications()
+    GATT.UART.Tx.handle.startNotifications()
+    .then(_ => {
+        console.log("Start reading...");
+        document.getElementById("btn_start").disabled = true;
+        document.getElementById("btn_stop").disabled = false;
+    })
+    .catch(error => {
+        console.log("[ERROR line: " + error.lineNumber + "] start(): " + error)
+    })
+}
+
+function stop() {
+    GATT.realTime.RTButtonStatus.handle.stopNotifications()
+    GATT.UART.Tx.handle.stopNotifications()
+    .then(_ => {
+        console.log("Stop reading...");
+        document.getElementById("btn_start").disabled = false;
+        document.getElementById("btn_stop").disabled = true;
+    })
+    .catch(error => {
+        console.log("[ERROR line: " + error.lineNumber + "] stop(): " + error)
+    })
+}
