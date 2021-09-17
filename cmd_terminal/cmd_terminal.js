476659d56d1d024df891e90efa15d36a1d82892b
Olly McBride OB <ollymcbride@openbionics.com>
2021-09-17 15:26:16 +0100

    Connect & subscribe to a GATT peripheral device

--- cmd_terminal/cmd_terminal.js
+++ cmd_terminal/cmd_terminal.js
@@ -1,9 +1,65 @@
+/**< Create an instance of the GATT manager to allow us to communicate with a BLE Peripheral */
 let GATT = new GATTmanager();
 
+/**< When the Connect/Disconnect button is clicked */
 document.getElementById("btn_connect").addEventListener("click", function() {
+    // if web BLE is available in this browser
     if (GATT.isWebBLEAvailable()) {
-        GATT.connect().then(_ => {
-            document.getElementById("label_dev").innerHTML = GATT.deviceName();
-        })
+        // if we are not currently connected to a BLE peripheral
+        if (!GATT.isConnected()) {
+            console.log("Connecting");
+            // show the 'Devices to connect to' dialog & try to connect to the selected device
+            GATT.connect().then(_ => {
+                // upon successful conneciton, set the device name & change the button to 'Disconnect
+                document.getElementById("label_dev").innerHTML = GATT.deviceDisplayName();
+                document.getElementById("btn_connect").innerHTML = "Disconnect"
+                
+                // DEBUG - subscribe to ButtonStatus & display in terminal
+                GATT.GATTtable.RTservice.RTButtonStatus.onValueChange( function(event) {
+                    document.getElementById("label_cmd_responses").innerHTML += event.target.value.getUint8(0).toString() + "<br>";
+                    let response_label_div = document.getElementById("cmd_responses_terminal");
+                    response_label_div.scrollTop = response_label_div.scrollHeight;
+                })
+
+            }).                
+            // else if connection failed
+            catch(error => {
+                console.log("Connection failed: " + error);
+                // reset the header to known values
+                document.getElementById("label_dev").innerHTML = "None";
+                document.getElementById("btn_connect").innerHTML = "Connect"
+            })
+        }
+        // else if we want to disconnect
+        else {
+            console.log("Disconnecting");
+            GATT.disconnect();
+            // reset the header to known values
+            document.getElementById("label_dev").innerHTML = GATT.deviceDisplayName();     // set to None by GATT
+            document.getElementById("btn_connect").innerHTML = "Connect"
+        }
     }
 })
+
+/**< Return true if str has the correct number of digits and is a valid hex string (ignore 0x) */
+function isValidHex(str, expected_len) {
+    if (str.length == expected_len)
+    {
+        if (str.match(/[0-9a-fA-F]{4}/))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+/**< If the Standard Request Cmd is valid, enable the Enter button, else disable it */
+document.getElementById("s_request_cmd").addEventListener("input", function(event) {
+    document.getElementById("s_request_enter").disabled = !isValidHex(event.target.value, 4);
+});
+
+/**< If the Large Request Cmd is valid, enable the Enter button, else disable it */
+document.getElementById("l_request_cmd").addEventListener("input", function(event) {
+    document.getElementById("l_request_enter").disabled = !isValidHex(event.target.value, 4);
+});
--- cmd_terminal/gatt.js
+++ cmd_terminal/gatt.js
@@ -3,21 +3,20 @@
 
 class GATTmanager {
     constructor() {
+        this.GATTtable = {};
         this.connected = false;
 
         // define the GATT table layout
-        this.GATTtable = {};
-        
-        this.GATTtable.NRTservice = new GATTservice(new OB_UUID("1007"));
-        this.GATTtable.NRTservice.addCharacteristic("NRTrequest", new GATTcharacteristic(new OB_UUID("2017")))
-        this.GATTtable.NRTservice.addCharacteristic("NRTResponse", new GATTcharacteristic(new OB_UUID("2018")))
-        this.GATTtable.NRTservice.addCharacteristic("NRTLargeRequest", new GATTcharacteristic(new OB_UUID("201e")))
-        this.GATTtable.NRTservice.addCharacteristic("NRTLargeResponse", new GATTcharacteristic(new OB_UUID("2019")))
+        this.addService("NRTservice", new OB_UUID("1007")).
+                addCharacteristic("NRTRequest", new OB_UUID("2017")).
+                addCharacteristic("NRTResponse", new OB_UUID("2018")).
+                addCharacteristic("NRTLargeRequest", new OB_UUID("201e")).
+                addCharacteristic("NRTLargeResponse", new OB_UUID("2019"));
 
-        this.GATTtable.RTservice = new GATTservice(new OB_UUID("1008"));
-        this.GATTtable.RTservice.addCharacteristic("RTButtonStatus", new GATTcharacteristic(new OB_UUID("201a")))
+        this.addService("RTservice", new OB_UUID("1008")).
+                addCharacteristic("RTButtonStatus", new OB_UUID("201a"));
 
-        
+
         // generate the connection options
         this.options = {
             optionalServices: [],
@@ -33,6 +32,12 @@ class GATTmanager {
         }
     }
 
+    /**< Assign a service to the GATT table */
+    addService(name, service_uuid) {
+        return this.GATTtable[name] = new GATTservice(service_uuid);
+    }
+
+
     /**< Return true if Web BLE is available on this browser */
     isWebBLEAvailable() {
         if (!navigator.bluetooth) {
@@ -42,6 +47,18 @@ class GATTmanager {
         return true;
     }
 
+    /**< Return true if we are currently connected to a BLE peripheral */
+    isConnected() {
+        return this.connected;
+    }
+
+    /**< Get the display name of the connected device */
+    deviceDisplayName() {
+        // TODO, read the friendly name of the Hero BLE module
+        return this.isConnected() ? "Hero BLE" : "None";
+    }
+
+    /**< Display the device selector window, then connect to the selected device */
     async connect() {
         // TODO, what happens if we are already connected?
         console.log("connect()");
@@ -54,19 +71,27 @@ class GATTmanager {
             
             console.log("Discovering services...")
             // get the representation of the GATT server on the remote device
-            const server = await this.BLEDevice.gatt.connect();    
+            this.server = await this.BLEDevice.gatt.connect();    
 
             // iterate through each of the predefined services in the local GATT table
             for (var service in this.GATTtable)
             {
-                // get the handle for the service
-                this.GATTtable[service].handle = await server.getPrimaryService(this.GATTtable[service].UUID.toString());
-
-                // iterate through each of the predefined characteristics in the local GATT table
-                for (var characteristic in this.GATTtable[service].charateristics)
+                // ensure the object is a service
+                if (this.GATTtable[service] instanceof GATTservice)
                 {
-                    // get the handle for the characteristic
-                    this.GATTtable[service].charateristics[characteristic].handle = await this.GATTtable[service].handle.getCharacteristic(this.GATTtable[service].charateristics[characteristic].UUID.toString());
+                    // get the handle for the service
+                    this.GATTtable[service].handle = await this.server.getPrimaryService(this.GATTtable[service].UUID.toString());
+                    
+                    // iterate through each of the predefined characteristics in the local GATT table
+                    for (var characteristic in this.GATTtable[service])
+                    {
+                        // ensure the object is a characteristic
+                        if (this.GATTtable[service][characteristic] instanceof GATTcharacteristic)
+                        {
+                            // get the handle for the characteristic
+                            this.GATTtable[service][characteristic].handle = await this.GATTtable[service].handle.getCharacteristic(this.GATTtable[service][characteristic].UUID.toString());
+                        }
+                    }
                 }
             }
 
@@ -78,10 +103,14 @@ class GATTmanager {
             console.log("Connection error: " + error)
         }
     }
-
-    deviceName() {
-        return "Hero BLE";
+    
+    /**< Disconnect from the BLE peripheral */
+    disconnect() {
+        this.server.disconnect();
+        this.connected = false;
     }
+
+
 }
 
 
@@ -104,11 +133,11 @@ class GATTservice extends GATTitem {
         super(long_uuid);
     }
 
-    charateristics = {};
-
     /**< Assign a characteristic to this service */
-    addCharacteristic(name, c) {
-        this.charateristics[name] = c;
+    addCharacteristic(name, characteristic_uuid) {
+        this[name] = new GATTcharacteristic(characteristic_uuid);
+
+        return this;    // allow this function to be chainable, to add multiple chars
     }
 }
 
@@ -121,45 +150,36 @@ class GATTcharacteristic extends GATTitem {
 
     /*< Read the current value of the characteristic */
     read() {
-        if (is_connected())
-        {
+        // if (is_connected())
+        // {
             // TODO, read the current value of the characteristic
-        }
+        // }
     }
 
     /**< Write a value to the characteristic */
     write(val) {
-        if (is_connected())
-        {
+        // if (is_connected())
+        // {
             try {
                 // TODO, convert val to Uint8Array
-                super.handle.writeValueWithoutResponse(val);
+                this.handle.writeValueWithoutResponse(val);
             } catch(error) {
                 console.log("ERROR - Unable to write to GATT item '%s' " + error, this.UUID)
             }
 
-        }
+        // }
     }
 
     /**< Add a callback to call when the characteristic value changes */
     onValueChange(callback) {
-        if (is_connected())
-        {
+        // if (is_connected())
+        // {
             try {
-                super.handle.addEventListener("characteristicvaluechanged", callback);
+                this.handle.addEventListener("characteristicvaluechanged", callback);
+                this.handle.startNotifications();
             } catch(error) {
                 console.log("ERROR - Unable to add onValueChange callback to GATT item '%s' " + error, this.UUID)
             }
-        }
+        // }
     }
 }
-
-
-
-// class GATT_OB  extends GATTitem{
-//     constructor(short_uuid) {
-//         super(new OB_UUID(short_uuid).toString());
-//     }
-// }
-
-
